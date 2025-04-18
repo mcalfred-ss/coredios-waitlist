6f20b6097261e42f3c294f6e136e8c5593a60721
coredios_games <mcalfredtabi275@gmail.com>
2025-04-17 19:38:35 -0700

    Remove extra mongoose.connect from sendEmails.js

--- sendEmails.js
+++ sendEmails.js
@@ -1,41 +1,29 @@
 // sendEmails.js
-require('dotenv').config();
-const mongoose = require('mongoose');
+const Email = require('./src/models/Email');
 const nodemailer = require('nodemailer');
 
-// 1) Connect to DB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, useUnifiedTopology: true
-});
-
-// 2) Model (same as server.js)
-const Email = mongoose.model('Email', new mongoose.Schema({
-  email: String,
-  dateJoined: Date
-}));
-
-// 3) Configure transporter
+// set up your SMTP transporter however you had it:
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'YOUR_SMTP_HOST',
+  port: 587,
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
   }
 });
 
-// 4) Fetch & send
-async function blast() {
-  const list = await Email.find().lean();
-  for (let { email } of list) {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: email,
-      subject: '🚀 Coredios Early Access!',
-      text: `Hey there! Thanks for joining our waitlist…`
-    });
-    console.log(`Sent to ${email}`);
-  }
-  mongoose.disconnect();
+async function sendConfirmation(emailAddress) {
+  // Save to Mongo (we already connected in server.js)
+  await Email.create({ address: emailAddress });
+  
+  // Send mail
+  await transporter.sendMail({
+    from: '"Coredios Team" <no-reply@coredios.com>',
+    to: emailAddress,
+    subject: 'Thanks for joining the waitlist!',
+    text: 'We’ll let you know as soon as we launch 🚀'
+  });
 }
 
-blast().catch(console.error);
+module.exports = sendConfirmation;
